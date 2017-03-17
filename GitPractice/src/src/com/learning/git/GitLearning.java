8b36d812ed1d05b6855aa4c879215ffaf12650ff
saranyasanthosh <saranyasanthosh@192.168.0.65>
2017-03-17 12:39:20 -0400

    Third output statement

--- GitPractice/src/src/com/learning/git/GitLearning.java
+++ GitPractice/src/src/com/learning/git/GitLearning.java
@@ -1,8 +1,10 @@
-package com.learning.git;
+package src.com.learning.git;
 
+import java.lang.*;
 public class GitLearning {
  public static void main(String[] args) {
 	System.out.println("Hello");
 	System.out.println("World");
+	System.out.println("Test 1");
 }
 }
